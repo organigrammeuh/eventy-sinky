44147cdc839bf13478494a878fd5b1031af5e8d3
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2026-04-29 21:50:09 +0300

    feat: find all event endpoint

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,4 +1,4 @@
-import { EventCreation, Event } from "@/lib/types";
+import { EventCreation, Event, Session, Speaker } from "@/lib/types";
 import { NextRequest, NextResponse } from "next/server";
 import { pool } from "@/lib/db";
 
@@ -58,4 +58,135 @@ export async function POST (
             {status : 500}
         );
     }
+}
+
+export async function GET(){
+    
+    let events : Event[] = [];
+
+    const findEventQuery = `
+        SELECT 
+            id, title, description, start_date, end_date, place
+        FROM event;
+        `;
+    
+    const findSessionQuery = `
+        SELECT
+            s.id, s.title, s.description,
+            s.start_date, s.end_date, s.capacity,
+            r.name
+        FROM session s
+        JOIN room r on s.id_room = r.id
+        WHERE
+            id_event = $1
+
+    `;
+
+    const findSpeakerQuery = `
+        SELECT
+            id, full_name, profile_picture_url, biography
+        FROM speaker s
+        JOIN session_speaker ss
+            ON ss.id_speaker = s.id
+        WHERE ss.id_session = $1
+    `;
+
+    const findSpeakerLinksQuery = `
+        SELECT 
+            url
+        FROM link
+        WHERE id_speaker = $1
+    `;
+
+    try{
+    
+        const findEventResult = await pool.query(findEventQuery);
+    
+    
+        for(const row of findEventResult.rows){
+
+            let event : Event = {
+                id: row.id,
+                description: row.description,
+                endDate: row.end_date,
+                startDate: row.start_date,
+                location: row.location,
+                title: row.title
+            };
+            
+            let eventSessions : Session[] = [];
+
+            const findSessionResult = await pool.query(
+                findSessionQuery, [event.id]
+            );
+
+            for(const session of findSessionResult.rows){
+                let fetchedSession : Session = {
+                     id: session.id,
+                     capacity: session.capacity,
+                     description: session.description,
+                     endTime: session.end_date,
+                     room: session.name,
+                     startTime: session.start_date,
+                     title: session.title
+                };
+
+                const findSpeakerResult = await pool.query(
+                    findSpeakerQuery , [session.id]
+                );
+                
+                const speakers : Speaker [] = [];
+
+                for(const speaker of findSpeakerResult.rows){
+                    
+                    let thisSpeaker : Speaker = {
+                        id: speaker.id,
+                        bio: speaker.biography,
+                        fullName: speaker.full_name,
+                        profilePicture: speaker.profile_picture_url
+                    };
+
+                    let links : string[] = [];
+
+                    const getSpeakerLinkResult = await pool.query(
+                        findSpeakerLinksQuery , [speaker.id]
+                    );
+
+                    for(const linkRow of getSpeakerLinkResult.rows){
+                        links.push(linkRow.url);
+                    }
+
+                    thisSpeaker.links = links;
+                    
+                    speakers.push(thisSpeaker);
+
+                }
+                fetchedSession.speakers = speakers;
+
+                eventSessions.push(fetchedSession)
+            }
+
+            event.sessions = eventSessions;
+
+            events.push(event);
+
+        }
+
+    } catch (err : any){
+        return NextResponse.json(
+            {
+                error : err.message,
+                message: 'Error when fecthing all the evnts'
+            },{
+                status: 500
+            }
+        )
+    }
+
+
+    return NextResponse.json(
+        events,
+        {status : 200}
+    );
+
 }
