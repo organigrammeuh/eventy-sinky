0ca84b4583976b46712b7ede65c5c35c752f21b3
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-05-02 13:12:25 +0300

    feat : type for question creation

--- types/question.ts
+++ types/question.ts
@@ -1,7 +1,15 @@
 export interface getQuestions {
   content: string,
-  name: string | null, sessionId: string,
+  name: string | null,
+  sessionId: string,
   createdAt: string,
   id: string,
   upvotes: number
+}
+
+export interface createQuestion {
+  content: string,
+  name?: string,
+  sessionId: string,
+  createdAt: string
 }
