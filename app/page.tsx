b4d7b61d42b754c4bc2c419875d77cf32c229384
Ania-su <antsaaa.randria@gmail.com>
2026-05-09 09:29:32 +0300

    fix: change navbar place to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
 import {SessionsPage} from "@/components/SessionPage";
 
 export default function Home() {
   return (
     <div>
-        <Navbar/>
         <Hero/>
         <SessionsPage/>
     </div>
