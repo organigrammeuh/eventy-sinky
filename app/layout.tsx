5eae662dee54e3a7d8b983b2f3e9d9b53d69561b
Ania-su <antsaaa.randria@gmail.com>
2026-06-29 16:46:21 +0300

    feat: footer section in all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { ThemeToggle } from "@/components/ThemeToggle";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <ThemeToggle />
+        <Footer/>
       </body>
     </html>
   );
